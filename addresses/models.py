363447ed9cb196d026e9bc664a78469cf0d6510b
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-11-26 10:28:48 +0300

    models oluşturuldu

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from users.models import User
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="addresses")
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.city}, {self.country}"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    school = models.CharField(max_length=150)
+    department = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
