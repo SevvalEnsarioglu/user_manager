6b74b021233aea4883239394731e0b7cfe55469f
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-11-26 10:47:40 +0300

    user views güncellendi

--- users/views.py
+++ users/views.py
@@ -14,12 +14,19 @@ class UserCreateView(APIView):
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
     
+"""serializer dediğimiz hem verinin dönüşümlerini hem de kontrollerini
+yapar. geçerliyse kaydediyoruz ve datayı dönüyoruz değilse hata fırlatıyoruz"""
+    
+
 #read all
 class UserListView(APIView):
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many = true)
         return Response(serializer.data)
+    
+"""tüm user kayıtlarını çekiyoruz. serializer ile dönüşümünü yapıp return ediyoruz"""
+
 
 #read single
 class UserDetailView(APIView):
@@ -31,6 +38,12 @@ class UserDetailView(APIView):
         serializer = UserSerializer(user)
         return Response(serializer.data)
     
+"""ilgili id ye sahip datayı user'a çekmeyi deneriz, ilgili id nin olduğu
+data yok ise  except ile user not found hatası fırlatırız.
+data var ise serializer ile kontrollerini ve düzenlemesini yapıp 
+response return ederiz"""
+
+
 #update
 class UserUpdateView(APIView):
     def put(self, request, pk):
@@ -44,6 +57,10 @@ class UserUpdateView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
     
+"""try ile ilgili id de nesnenin olup olmadığına bakarız. 
+olduğu senaryoda eğer serializer ile yapılan kontroller ve düzenlemeler
+geçerli ise response return ederiz kaydederiz"""
+    
 #delete
 class UserDeleteView(APIView):
     def delete(self, request, pk):
@@ -53,9 +70,12 @@ class UserDeleteView(APIView):
             return Response({"error": "User not found"}, status=404)
         user.delete()
         return Response(status=204)
-    
+
+"""ilgili id nin bulunup bulunmadığını kontrol ederiz, yoksa not found
+varsa datasını sileriz""" 
 
 
+#NOTLARIM
 """
 user = User.objects.get(pk=pk) dediğimizde burada Django ORM kullanıyoruz.
 User.objects User modelinin tüm kayıtlarını temsil eder. 
